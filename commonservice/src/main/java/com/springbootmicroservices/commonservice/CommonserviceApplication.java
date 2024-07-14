f89f3636cc9681fa7c777850540c86525be171af
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-07-14 18:17:33 +0300

    Task 8 : Define Common Service

---
+++ commonservice/src/main/java/com/springbootmicroservices/commonservice/CommonserviceApplication.java
@@ -0,0 +1,13 @@
+package com.springbootmicroservices.commonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonserviceApplication.class, args);
+	}
+
+}
---
+++ commonservice/src/test/java/com/springbootmicroservices/commonservice/CommonserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootmicroservices.commonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
