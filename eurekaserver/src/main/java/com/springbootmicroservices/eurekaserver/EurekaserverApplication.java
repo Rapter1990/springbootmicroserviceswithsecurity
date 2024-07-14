26ed5d1c4bdb14833cc51ed8fc54927b63cdd007
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-07-14 14:43:18 +0300

    Task 7 : Add Java Doc for EurekaserverApplication and EurekaserverApplicationTests

--- eurekaserver/src/main/java/com/springbootmicroservices/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/springbootmicroservices/eurekaserver/EurekaserverApplication.java
@@ -4,10 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Main class named {@link EurekaserverApplication} for the Eureka Server application.
+ * The {@link EnableEurekaServer} annotation activates the Eureka Server in the application
+ * This class sets up and runs a Eureka Server for service discovery. Eureka Server
+ * is part of the Netflix OSS suite and provides a REST-based interface for services
+ * to register and discover each other.
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaserverApplication {
 
+	/**
+	 * Main method to run the Spring Boot application.
+	 *
+	 * @param args Command-line arguments passed during the application startup.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaserverApplication.class, args);
 	}
--- eurekaserver/src/test/java/com/springbootmicroservices/eurekaserver/EurekaserverApplicationTests.java
+++ eurekaserver/src/test/java/com/springbootmicroservices/eurekaserver/EurekaserverApplicationTests.java
@@ -7,12 +7,24 @@ import org.springframework.context.ApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * Test class for the {@link EurekaserverApplication}.
+ * This class contains unit tests to ensure that the application context loads correctly
+ * and that the Spring Boot application starts up without any issues.
+ * The {@link SpringBootTest} annotation indicates that this is a Spring Boot test
+ * that will start the full application context.
+ */
 @SpringBootTest
 class EurekaserverApplicationTests {
 
 	@Autowired
 	private ApplicationContext applicationContext;
 
+	/**
+	 * Test to verify that the application context loads correctly.
+	 * This test method asserts that the {@link #applicationContext} is not null,
+	 * indicating that the Spring Boot application context has been successfully loaded.
+	 */
 	@Test
 	void contextLoads() {
 		assertThat(applicationContext).isNotNull();
