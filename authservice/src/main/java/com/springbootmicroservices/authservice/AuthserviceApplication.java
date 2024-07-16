85b15970467384bc503b511b7a2f27f778517daf
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-07-16 08:40:07 +0300

    Task 22 : Add User Service and Auth Service

---
+++ authservice/src/main/java/com/springbootmicroservices/authservice/AuthserviceApplication.java
@@ -0,0 +1,13 @@
+package com.springbootmicroservices.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthserviceApplication.class, args);
+	}
+
+}
